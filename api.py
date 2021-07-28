34daa944a48e60e051f5d9c087ec6d820cd39a3f
Данила Богославцев <da.png@yandex.ru>
2021-07-28 22:47:02 +0300

    Included video player to project

--- api.py
+++ api.py
@@ -2,17 +2,19 @@ import shutil
 import filetype
 from uuid import uuid4
 from typing import List
-from starlette.responses import StreamingResponse
+
+from starlette.requests import Request
+from starlette.responses import StreamingResponse, HTMLResponse
 from starlette.templating import Jinja2Templates
 from fastapi import FastAPI, Form, UploadFile, File, APIRouter, BackgroundTasks, HTTPException
 
 
-from schemas import UploadVideo, GetVideo, Message
+from schemas import UploadVideo, GetVideo, Message, GetListVideo
 from models import Video, User
 from services import save_video
 
 video_router = APIRouter()
-
+templates = Jinja2Templates(directory="templates")
 
 @video_router.post("/")
 async def create_video(
@@ -30,5 +32,14 @@ async def get_video(video_pk: int):
     file = await Video.objects.select_related('user').get(pk=video_pk)
     file_like = open(file.dict().get('file'), mode="rb")
     return StreamingResponse(file_like, media_type="video/mp4")
-#просмотр видео только через postman
 
+
+@video_router.get("/user/{user_pk}", response_model=List[GetListVideo])
+async def get_list_video(user_pk: int):
+    video_list = await Video.objects.filter(user=user_pk).all()
+    return video_list
+
+
+@video_router.get("/index/{video_pk}", response_class=HTMLResponse)
+async def get_video(request: Request, video_pk: int):
+    return templates.TemplateResponse("index.html", {"request": request, "path": video_pk})
